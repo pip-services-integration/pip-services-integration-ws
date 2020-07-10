7a87b978b9f56124a76fc91e4592f8d04625a090
Sergey Seroukhov <seroukhov@gmail.com>
2020-07-10 12:51:51 -0700

    Renamed workflows to processes

--- checkout.sh
+++ checkout.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-git clone git@github.com:pip-services-integration/pip-libs-workflows-node.git
+git clone git@github.com:pip-services-integration/pip-libs-processes-node.git
 
-git clone git@github.com:pip-services-integration/pip-services-workflowstates-node.git
+git clone git@github.com:pip-services-integration/pip-services-processstates-node.git
 git clone git@github.com:pip-services-integration/pip-services-duplicates-node.git
 git clone git@github.com:pip-services-integration/pip-services-mappings-node.git
 git clone git@github.com:pip-services-integration/pip-services-changescopes-node.git
 git clone git@github.com:pip-services-integration/pip-services-search-node.git
 
 git clone git@github.com:pip-services-integration/pip-clients-tempblobs-node.git
-git clone git@github.com:pip-services-integration/pip-clients-workflowstates-node.git
+git clone git@github.com:pip-services-integration/pip-clients-processstates-node.git
 git clone git@github.com:pip-services-integration/pip-clients-duplicates-node.git
 git clone git@github.com:pip-services-integration/pip-clients-mappings-node.git
 git clone git@github.com:pip-services-integration/pip-clients-changescopes-node.git
