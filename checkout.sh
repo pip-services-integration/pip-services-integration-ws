cb0af2cccc94017ffc50a0c13b1da88a1fffe50c
Sergey Seroukhov <seroukhov@gmail.com>
2020-07-18 10:18:56 -0700

    Renamed duplicates with retries

--- checkout.sh
+++ checkout.sh
@@ -3,14 +3,14 @@
 git clone git@github.com:pip-services-integration/pip-libs-processes-node.git
 
 git clone git@github.com:pip-services-integration/pip-services-processstates-node.git
-git clone git@github.com:pip-services-integration/pip-services-duplicates-node.git
+git clone git@github.com:pip-services-integration/pip-services-retries-node.git
 git clone git@github.com:pip-services-integration/pip-services-mappings-node.git
 git clone git@github.com:pip-services-integration/pip-services-changescopes-node.git
 git clone git@github.com:pip-services-integration/pip-services-search-node.git
 
 git clone git@github.com:pip-services-integration/pip-clients-tempblobs-node.git
 git clone git@github.com:pip-services-integration/pip-clients-processstates-node.git
-git clone git@github.com:pip-services-integration/pip-clients-duplicates-node.git
+git clone git@github.com:pip-services-integration/pip-clients-retries-node.git
 git clone git@github.com:pip-services-integration/pip-clients-mappings-node.git
 git clone git@github.com:pip-services-integration/pip-clients-changescopes-node.git
 git clone git@github.com:pip-services-integration/pip-clients-search-node.git
